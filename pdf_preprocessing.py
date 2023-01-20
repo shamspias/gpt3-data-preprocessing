18a5c2874b2c05a0286c0921a58553c333afe9b7
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 02:18:19 +0600

    feat: pdf

---
+++ pdf_preprocessing.py
@@ -0,0 +1,23 @@
+import PyPDF2
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+
+# Open the PDF file
+with open('book.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    text = ""
+    # Iterate through each page of the PDF
+    for page in range(pdf_reader.numPages):
+        text += pdf_reader.getPage(page).extractText()
+
+# Tokenize the text
+tokens = word_tokenize(text)
+
+# Remove stop words and punctuation
+stop_words = set(stopwords.words('english'))
+tokens = [token.lower() for token in tokens if token not in stop_words and token not in string.punctuation]
+
+# Save the preprocessed data as a JSON file
+with open('book_data.json', 'w') as json_file:
+    json.dump(tokens, json_file)
